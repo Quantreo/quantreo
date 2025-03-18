9764678d48c0e701f1d87856ec671afbdd047ae8
Lucas Inglese <lucas.inglese.pro@gmail.com>
2025-03-18 15:09:53 +0100

    Add Datasets

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='quantreo',  # The name of the package as it will appear on PyPI.
-    version='0.0.7',
+    version='0.0.9',
     description='Python library for quantitative trading',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -19,8 +19,11 @@ setup(
               "quantreo.features_engineering.market_regime",
               "quantreo.features_engineering.math",
               "quantreo.features_engineering.trend",
-              "quantreo.features_engineering.volatility"],
+              "quantreo.features_engineering.volatility",
+              "quantreo.datasets"],
     install_requires=["numpy", "pandas", "numba"],
+    include_package_data=True,
+    package_data={'quantreo.datasets': ['*.csv']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
