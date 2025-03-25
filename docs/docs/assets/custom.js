1e1cd911fcdf8e63cac474ccf0f293fb6dd055ea
Lucas Inglese <lucas.inglese.pro@gmail.com>
2025-03-25 15:05:20 +0100

    modif newsletter image

--- docs/docs/assets/custom.js
+++ docs/docs/assets/custom.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         imgDiv.classList.add("sidebar-image");
         imgDiv.innerHTML = `
             <a href="https://www.quantreo.com" target="_blank" style="text-decoration: none;">
-                <img src="/assets/pub_aqp.webp" class="sidebar-img" style="width: 80%; border-radius: 10px; margin-top: 50px; display: block; transition: transform 0.3s ease;">
+                <img src="/assets/newsletter.webp" class="sidebar-img" style="width: 80%; border-radius: 10px; margin-top: 50px; display: block; transition: transform 0.3s ease;">
             </a>
         `;
         sidebarInner.appendChild(imgDiv);
